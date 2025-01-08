c81be3d2f9541f3a57b6b99e5b9b813450a817b5
KelpinHarr <calvinharsono07@gmail.com>
2025-01-08 14:27:14 +0700

    Create target_muscle_detail.dart

---
+++ lib/ui/target_muscle_detail.dart
@@ -0,0 +1,14 @@
+import 'package:fit_now/models/Workout.dart';
+import 'package:flutter/material.dart';
+
+class TargetMuscleDetailPage extends StatelessWidget {
+  final Workout workout;
+  const TargetMuscleDetailPage({super.key, required this.workout});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
