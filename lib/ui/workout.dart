3d80e13f022f2697701d306f271e47e26b981d70
KelpinHarr <calvinharsono07@gmail.com>
2025-01-22 07:56:19 +0700

    analysis video 80%

--- lib/ui/workout.dart
+++ lib/ui/workout.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fit_now/bloc/workout_bloc.dart';
 import 'package:fit_now/components/bottom_navbar.dart';
@@ -5,10 +8,14 @@ import 'package:fit_now/constants.dart';
 import 'package:fit_now/models/Video.dart';
 import 'package:fit_now/ui/chat.dart';
 import 'package:fit_now/ui/workout_detail.dart';
+import 'package:fit_now/widgets/text_field_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 
 class WorkoutPage extends StatefulWidget {
   final String email;
@@ -22,6 +29,10 @@ class _WorkoutPageState extends State<WorkoutPage> {
   int _currentIndex = 1;
   int _selectedFilterIndex = 0;
   Future<List<Video>>? _futureVideo;
+  File? _video;
+  final ImagePicker picker = ImagePicker();
+  late final GenerativeModel _model, _visionModel;
+  late final ChatSession _chat;
 
   final List<String> filters = [
     'Strength Training',
@@ -33,6 +44,285 @@ class _WorkoutPageState extends State<WorkoutPage> {
   void initState(){
     super.initState();
     _futureVideo = _fetchVideoURL();
+
+    _model = GenerativeModel(
+      model: 'gemini-1.5-pro', 
+      apiKey: 'AIzaSyAKxi3WF9J_CMl2H0SKlrbJIH5T4UF-Zgc'
+    );
+
+    _visionModel = GenerativeModel(
+      model: 'gemini-vision-pro', 
+      apiKey: 'AIzaSyAKxi3WF9J_CMl2H0SKlrbJIH5T4UF-Zgc'
+    );
+
+    _chat = _model.startChat();
+  }
+
+  Future<void> takeVideo(String name) async {
+    try {
+      var video = await picker.pickVideo(source: ImageSource.camera);
+
+      if (video == null){
+        return;
+      }
+
+      setState(() {
+        _video = File(video.path);
+      });
+
+      _getAnalysisFromGemini(_video, name);
+    }
+    catch(e){
+      Fluttertoast.showToast(
+        msg: 'Error :$e',
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        textColor: Colors.white,
+        backgroundColor: Colors.orange,
+        fontSize: 14
+      );
+    }
+  }
+
+  Future<void> _getAnalysisFromGemini(String videoPath, String workoutName) async {
+    try {
+      final analysisPrompt = """
+        Gemini, you are a workout personal trainer. You are the trainer for $workoutName. You must analyze the style and how user do the $workoutName. 
+
+        This is the workout video: $videoPath
+
+        Try to analyze and give a suggestion on how the player do the $workoutName from this video. Add the details description and suggestion from this video. Add some score for the $workoutName style and technique analysis both from 0 to 100.
+
+        Make the result into a JSON format with schema like this schema:
+        "{
+          "response_suggestion": {
+            "type": "string"
+          },
+          "style_score": {
+            "type": "integer"
+          },
+          "technique_score": {
+            "type": "string"
+          }
+        }"
+
+        Put the suggestion and analysis in the response_suggestion field, the style score from 0 to 100 into style_score field, and technique score from 0 to 100 into technique_score field.
+      """;
+
+      var content = Content.text(analysisPrompt);
+      final response = await _chat.sendMessage(content);
+
+      String cleanedResponse = response.text!
+          .replaceAll('\n', '')
+          .replaceAll('\n\n', '')
+          .replaceAll('\r', '')
+          .replaceAll('*', '')
+          .replaceAll('**', '')
+          .replaceFirst('```json', '')
+          .replaceAll('```', '')
+          .replaceFirst('JSON', '')
+          .trim();
+      
+      final analysisResult = cleanedResponse;
+
+      Map<String, dynamic> analysisData = jsonDecode(analysisResult);
+
+      _displayAnalysisResult(analysisData['response_suggestion'], analysisData['style_score'], analysisData['technique_score']);
+
+    }
+    catch (e){
+      Fluttertoast.showToast(
+        msg: 'Error: $e',
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        textColor: Colors.white,
+        backgroundColor: Colors.orange,
+        fontSize: 14
+      );
+    }
+  }
+
+  Future<void> _displayAnalysisResult(String response_suggestions, int style_score, int technique_score) async {
+    showDialog(
+      context: context, 
+      builder: (BuildContext context) => AlertDialog(
+        title: Text(
+          'Analysis Result',
+          style: TextStyle(
+            fontFamily: 'ReadexPro-Medium'
+          ),
+        ),
+        content: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Style Score: ',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'ReadexPro-Medium'
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    '$style_score/100',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'ReadexPro-Medium'
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Text(
+                    'Technique Score: ',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'ReadexPro-Medium'
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    '$technique_score/100',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'ReadexPro-Medium'
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Response Suggestions: ',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10,),
+                  Expanded(
+                    child: Text(
+                      response_suggestions,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 50,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+
+  void _showDialog() async {
+    final TextEditingController _workoutNameController = TextEditingController();
+
+    showDialog(
+      context: context, 
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text(
+            'Workout Name',
+            style: TextStyle(
+              fontFamily: 'ReadexPro-Medium'
+            ),
+          ),
+          backgroundColor: white,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Input workout name to analyze your move',
+                style: TextStyle(
+                  fontFamily: 'ReadexPro-Medium'
+                ),
+              ),
+              SizedBox(height: 10),
+              TextFieldContainer(
+                child: TextFormField(
+                  controller: _workoutNameController,
+                  cursorColor: orange,
+                  decoration: InputDecoration(
+                    icon: Icon(
+                      Iconsax.weight_1,
+                      color: orange,
+                    ),
+                    hintText: 'Workout Name',
+                    hintStyle: TextStyle(
+                      fontFamily: 'ReadexPro-Medium',
+                    ),
+                    border: InputBorder.none
+                  )
+                ),
+              )
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              }, 
+              child: Text(
+                'Cancel',
+                style: TextStyle(
+                  color: blue
+                ),
+              )
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                String workoutName = _workoutNameController.text.trim();
+                if (workoutName.isNotEmpty){
+                  Navigator.of(context).pop();
+                  await takeVideo(workoutName);
+                }
+                else {
+                  Fluttertoast.showToast(
+                    msg: 'Please enter workout name.',
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.BOTTOM,
+                    backgroundColor: Colors.red,
+                    textColor: Colors.white,
+                    fontSize: 14,
+                  );
+                }
+              }, 
+              child: Text(
+                'Submit',
+                style: TextStyle(
+                  color: white
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: blue
+              ),
+            )
+          ],
+        );
+      }
+    );
   }
 
   Future<List<Video>> _fetchVideoURL() async {
@@ -134,7 +424,7 @@ class _WorkoutPageState extends State<WorkoutPage> {
                 Padding(
                   padding: EdgeInsets.only(top: 20, right: 20),
                   child: IconButton(
-                    onPressed: (){}, 
+                    onPressed: _showDialog,
                     icon: Icon(Iconsax.camera)
                   ),
                 )
